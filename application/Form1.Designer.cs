5feda99b6367a05886bebbf03471a0803b0f61b8
epyon0 <epyon0@proton.me>
2026-06-23 16:52:31 -0500

    Add files via upload

---
+++ application/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace application
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            outputRichtextbox = new RichTextBox();
+            minButton = new Button();
+            maxButton = new Button();
+            closeButton = new Button();
+            commCombobox = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            speedCombobox = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            databitsNumericUpDown = new NumericUpDown();
+            stopbitsNumericUpDown = new NumericUpDown();
+            parityCombobox = new ComboBox();
+            flowcontrolCombobox = new ComboBox();
+            connectButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)databitsNumericUpDown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)stopbitsNumericUpDown).BeginInit();
+            SuspendLayout();
+            // 
+            // outputRichtextbox
+            // 
+            outputRichtextbox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            outputRichtextbox.BackColor = Color.Black;
+            outputRichtextbox.BorderStyle = BorderStyle.None;
+            outputRichtextbox.ForeColor = Color.Lime;
+            outputRichtextbox.Location = new Point(200, 31);
+            outputRichtextbox.Name = "outputRichtextbox";
+            outputRichtextbox.ReadOnly = true;
+            outputRichtextbox.Size = new Size(743, 397);
+            outputRichtextbox.TabIndex = 0;
+            outputRichtextbox.Text = "";
+            outputRichtextbox.LinkClicked += richTextBox1_LinkClicked;
+            outputRichtextbox.KeyPress += outputRichtextbox_KeyPress;
+            // 
+            // minButton
+            // 
+            minButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            minButton.BackColor = Color.Black;
+            minButton.FlatStyle = FlatStyle.Flat;
+            minButton.Font = new Font("Lucida Console", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            minButton.ForeColor = Color.Lime;
+            minButton.Location = new Point(880, 0);
+            minButton.Name = "minButton";
+            minButton.Size = new Size(25, 25);
+            minButton.TabIndex = 1;
+            minButton.Text = "—";
+            minButton.UseVisualStyleBackColor = false;
+            minButton.Click += minButton_Click;
+            // 
+            // maxButton
+            // 
+            maxButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            maxButton.BackColor = Color.Black;
+            maxButton.FlatStyle = FlatStyle.Flat;
+            maxButton.Font = new Font("Lucida Console", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            maxButton.ForeColor = Color.Lime;
+            maxButton.Location = new Point(905, 0);
+            maxButton.Name = "maxButton";
+            maxButton.Size = new Size(25, 25);
+            maxButton.TabIndex = 2;
+            maxButton.Text = "⧠";
+            maxButton.UseVisualStyleBackColor = false;
+            maxButton.Click += maxButton_Click;
+            // 
+            // closeButton
+            // 
+            closeButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            closeButton.BackColor = Color.Black;
+            closeButton.FlatStyle = FlatStyle.Flat;
+            closeButton.Font = new Font("Lucida Console", 8.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            closeButton.ForeColor = Color.Lime;
+            closeButton.Location = new Point(930, 0);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new Size(25, 25);
+            closeButton.TabIndex = 3;
+            closeButton.Text = "╳";
+            closeButton.UseVisualStyleBackColor = false;
+            closeButton.Click += closeButton_Click;
+            // 
+            // commCombobox
+            // 
+            commCombobox.BackColor = Color.Black;
+            commCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            commCombobox.FlatStyle = FlatStyle.Flat;
+            commCombobox.ForeColor = Color.Lime;
+            commCombobox.FormattingEnabled = true;
+            commCombobox.Location = new Point(114, 4);
+            commCombobox.Name = "commCombobox";
+            commCombobox.Size = new Size(80, 19);
+            commCombobox.TabIndex = 4;
+            commCombobox.SelectedIndexChanged += commCombobox_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 11);
+            label1.TabIndex = 5;
+            label1.Text = "Device:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 32);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 11);
+            label2.TabIndex = 6;
+            label2.Text = "Speed:";
+            // 
+            // speedCombobox
+            // 
+            speedCombobox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            speedCombobox.AutoCompleteSource = AutoCompleteSource.ListItems;
+            speedCombobox.BackColor = Color.Black;
+            speedCombobox.FlatStyle = FlatStyle.Flat;
+            speedCombobox.ForeColor = Color.Lime;
+            speedCombobox.FormattingEnabled = true;
+            speedCombobox.Items.AddRange(new object[] { "50", "75", "110", "134", "150", "200", "300", "600", "1200", "1800", "2400", "4800", "9600", "19200", "28800", "38400", "57600", "76800", "115200", "230400", "460800", "576000", "921600" });
+            speedCombobox.Location = new Point(114, 29);
+            speedCombobox.Name = "speedCombobox";
+            speedCombobox.Size = new Size(80, 19);
+            speedCombobox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 11);
+            label3.TabIndex = 8;
+            label3.Text = "Data Bits:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 82);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 11);
+            label4.TabIndex = 9;
+            label4.Text = "Stop Bits:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 107);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 11);
+            label5.TabIndex = 10;
+            label5.Text = "Parity:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 132);
+            label6.Name = "label6";
+            label6.Size = new Size(96, 11);
+            label6.TabIndex = 11;
+            label6.Text = "Flow Control:";
+            // 
+            // databitsNumericUpDown
+            // 
+            databitsNumericUpDown.BackColor = Color.Black;
+            databitsNumericUpDown.BorderStyle = BorderStyle.None;
+            databitsNumericUpDown.ForeColor = Color.Lime;
+            databitsNumericUpDown.Location = new Point(114, 58);
+            databitsNumericUpDown.Name = "databitsNumericUpDown";
+            databitsNumericUpDown.Size = new Size(80, 14);
+            databitsNumericUpDown.TabIndex = 12;
+            databitsNumericUpDown.TextAlign = HorizontalAlignment.Right;
+            databitsNumericUpDown.Value = new decimal(new int[] { 8, 0, 0, 0 });
+            // 
+            // stopbitsNumericUpDown
+            // 
+            stopbitsNumericUpDown.BackColor = Color.Black;
+            stopbitsNumericUpDown.BorderStyle = BorderStyle.None;
+            stopbitsNumericUpDown.DecimalPlaces = 1;
+            stopbitsNumericUpDown.ForeColor = Color.Lime;
+            stopbitsNumericUpDown.Increment = new decimal(new int[] { 5, 0, 0, 65536 });
+            stopbitsNumericUpDown.Location = new Point(114, 79);
+            stopbitsNumericUpDown.Maximum = new decimal(new int[] { 2, 0, 0, 0 });
+            stopbitsNumericUpDown.Name = "stopbitsNumericUpDown";
+            stopbitsNumericUpDown.Size = new Size(80, 14);
+            stopbitsNumericUpDown.TabIndex = 13;
+            stopbitsNumericUpDown.TextAlign = HorizontalAlignment.Right;
+            stopbitsNumericUpDown.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // parityCombobox
+            // 
+            parityCombobox.BackColor = Color.Black;
+            parityCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            parityCombobox.FlatStyle = FlatStyle.Flat;
+            parityCombobox.ForeColor = Color.Lime;
+            parityCombobox.FormattingEnabled = true;
+            parityCombobox.Items.AddRange(new object[] { "None", "Odd", "Even", "Mark", "Space" });
+            parityCombobox.Location = new Point(114, 104);
+            parityCombobox.Name = "parityCombobox";
+            parityCombobox.Size = new Size(80, 19);
+            parityCombobox.TabIndex = 14;
+            // 
+            // flowcontrolCombobox
+            // 
+            flowcontrolCombobox.BackColor = Color.Black;
+            flowcontrolCombobox.DropDownStyle = ComboBoxStyle.DropDownList;
+            flowcontrolCombobox.FlatStyle = FlatStyle.Flat;
+            flowcontrolCombobox.ForeColor = Color.Lime;
+            flowcontrolCombobox.FormattingEnabled = true;
+            flowcontrolCombobox.Items.AddRange(new object[] { "None", "XON/XOFF", "RTS/CTS", "DSR/DTR" });
+            flowcontrolCombobox.Location = new Point(114, 129);
+            flowcontrolCombobox.Name = "flowcontrolCombobox";
+            flowcontrolCombobox.Size = new Size(80, 19);
+            flowcontrolCombobox.TabIndex = 15;
+            // 
+            // connectButton
+            // 
+            connectButton.FlatStyle = FlatStyle.Flat;
+            connectButton.Location = new Point(12, 154);
+            connectButton.Name = "connectButton";
+            connectButton.Size = new Size(182, 23);
+            connectButton.TabIndex = 16;
+            connectButton.Text = "Connect";
+            connectButton.UseVisualStyleBackColor = true;
+            connectButton.Click += connectButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 11F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(955, 440);
+            Controls.Add(connectButton);
+            Controls.Add(flowcontrolCombobox);
+            Controls.Add(parityCombobox);
+            Controls.Add(stopbitsNumericUpDown);
+            Controls.Add(databitsNumericUpDown);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(speedCombobox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(commCombobox);
+            Controls.Add(closeButton);
+            Controls.Add(maxButton);
+            Controls.Add(minButton);
+            Controls.Add(outputRichtextbox);
+            Font = new Font("Lucida Console", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = Color.Lime;
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Shown += Form1_Shown;
+            MouseDoubleClick += Form1_MouseDoubleClick;
+            ((System.ComponentModel.ISupportInitialize)databitsNumericUpDown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)stopbitsNumericUpDown).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox outputRichtextbox;
+        private Button minButton;
+        private Button maxButton;
+        private Button closeButton;
+        private ComboBox commCombobox;
+        private Label label1;
+        private Label label2;
+        private ComboBox speedCombobox;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private NumericUpDown databitsNumericUpDown;
+        private NumericUpDown stopbitsNumericUpDown;
+        private ComboBox parityCombobox;
+        private ComboBox flowcontrolCombobox;
+        private Button connectButton;
+    }
+}
---
+++ application/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+
+namespace application
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            // click and drag form
+
+            if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)
+            {
+                m.Result = (IntPtr)HTCAPTION;
+            }
+
+            // resize form with mouse
+            if (m.Msg == 0x84) // WM_NCHITTEST
+            {
+                Point cursorPos = PointToClient(Cursor.Position);
+                int borderSize = 10; // Adjust sensitivity
+
+                if (cursorPos.X < borderSize && cursorPos.Y < borderSize)
+                    m.Result = (IntPtr)16; // HTTOPLEFT
+                else if (cursorPos.X > Width - borderSize && cursorPos.Y < borderSize)
+                    m.Result = (IntPtr)17; // HTTOPRIGHT
+                else if (cursorPos.X < borderSize && cursorPos.Y > Height - borderSize)
+                    m.Result = (IntPtr)15; // HTBOTTOMLEFT
+                else if (cursorPos.X > Width - borderSize && cursorPos.Y > Height - borderSize)
+                    m.Result = (IntPtr)13; // HTBOTTOMRIGHT
+                else if (cursorPos.X < borderSize)
+                    m.Result = (IntPtr)10; // HTLEFT
+                else if (cursorPos.X > Width - borderSize)
+                    m.Result = (IntPtr)11; // HTRIGHT
+                else if (cursorPos.Y < borderSize)
+                    m.Result = (IntPtr)12; // HTTOP
+                else if (cursorPos.Y > Height - borderSize)
+                    m.Result = (IntPtr)14; // HTBOTTOM
+            }
+        }
+        private void output(string text)
+        {
+            outputRichtextbox.Invoke(new Action(() =>
+            {
+                outputRichtextbox.AppendText(text + Environment.NewLine);
+                outputRichtextbox.SelectionStart = outputRichtextbox.TextLength;
+                outputRichtextbox.ScrollToCaret();
+                this.Refresh();
+            }));
+        }
+
+        private void debug(string text, [CallerMemberName] string caller = "", [CallerLineNumber] int line = 0)
+        {
+            DateTime now = DateTime.Now;
+            output(now.ToString("yyyy-MM-ddTHH:mm:ss.fff K", CultureInfo.InvariantCulture) + $" | {caller}:{line} | " + text);
+        }
+
+        private void getSerialDevices()
+        {
+            commCombobox.Items.Clear();
+            debug("Getting COMM devices");
+            List<string> commDevices = SerialPort.GetPortNames().ToList<string>();
+            foreach (string commDevice in commDevices)
+            {
+                commCombobox.Items.Add(commDevice);
+            }
+
+            if (commCombobox.Items.Count > 0)
+            {
+                commCombobox.SelectedIndex = 0;
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            sp = (SerialPort)sender;
+            string recvData = sp.ReadExisting();
+            debug($"RECV: {recvData}");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            debug("Form loaded");
+            getSerialDevices();
+            if (speedCombobox.Items.Count > 11) { speedCombobox.SelectedIndex = 12; }
+            if (parityCombobox.Items.Count > 0) { parityCombobox.SelectedIndex = 0; }
+            if (flowcontrolCombobox.Items.Count > 1) { flowcontrolCombobox.SelectedIndex = 1; };
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void maxButton_Click(object sender, EventArgs e)
+        {
+            if (maxButton.Text == "⧠")
+            {
+                debug("Maximize form");
+                this.WindowState = FormWindowState.Maximized;
+                maxButton.Text = "⧉";
+                return;
+            }
+
+            if (maxButton.Text == "⧉")
+            {
+                debug("Restore form");
+                this.WindowState = FormWindowState.Normal;
+                maxButton.Text = "⧠";
+                return;
+            }
+        }
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            maxButton_Click(sender, e);
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = e.LinkText,
+                UseShellExecute = true
+            });
+        }
+
+        private void minButton_Click(object sender, EventArgs e)
+        {
+            debug("Minimize form");
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void commCombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            debug($"Port selected: {commCombobox.Text}");
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (connectButton.Text == "Connect")
+            {
+                string device = commCombobox.Text;
+                int baudRate = 9600;
+                Parity parity = Parity.None;
+                int dataBits = 8;
+                StopBits stopBits = StopBits.None;
+
+                try { baudRate = int.Parse(speedCombobox.Text); } catch (Exception ex) { debug($"EXCEPTION: [SPEED/BAUD RATE]: {ex.Message}"); }
+                try
+                {
+                    switch (parityCombobox.Text)
+                    {
+                        case "None":
+                            parity = Parity.None; break;
+                        case "Odd":
+                            parity = Parity.Odd; break;
+                        case "Even":
+                            parity = Parity.Even; break;
+                        case "Mark":
+                            parity = Parity.Mark; break;
+                        case "Space":
+                            parity = Parity.Space; break;
+                    }
+                }
+                catch (Exception ex) { debug($"EXCEPTION: [PARITY]: {ex.Message}"); }
+                try { dataBits = int.Parse(databitsNumericUpDown.Value.ToString()); } catch (Exception ex) { debug($"EXCEPTION: [DATA BITS]: {ex.Message}"); }
+                try
+                {
+                    switch (stopbitsNumericUpDown.Value)
+                    {
+                        case 0M:
+                            stopBits = StopBits.None; break;
+                        case 1M:
+                            stopBits = StopBits.One; break;
+                        case 1.5M:
+                            stopBits = StopBits.OnePointFive; break;
+                        case 2M:
+                            stopBits = StopBits.Two; break;
+                    }
+                }
+                catch (Exception ex) { debug($"EXCEPTION: [STOP BITS]: {ex.Message}"); }
+
+                sp = new SerialPort(device, baudRate, parity, dataBits, stopBits);
+                sp.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+                debug($"Opening serial interface: {device}");
+                connectButton.Text = "Disconnect";
+                if (sp.IsOpen)
+                {
+                    debug($"Serial port [{device}] is already open");
+                    return;
+                }
+
+                sp.Open();
+                return;
+            }
+
+            if (connectButton.Text == "Disconnect")
+            {
+                debug($"Closing serial interface: {commCombobox.Text}");
+                if (sp.IsOpen)
+                {
+                    sp.Close();
+                }
+                else
+                {
+                    debug($"Serial interface [{commCombobox.Text}] is not open");
+                }
+
+                connectButton.Text = "Connect";
+                return;
+            }
+        }
+
+        private void outputRichtextbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (sp.IsOpen)
+            {
+                if (e.KeyChar == '\r' || e.KeyChar == '\n')
+                {
+                    sp.Write("\r\n");
+                } else
+                {
+                    sp.Write(e.KeyChar.ToString());
+                }
+            }
+        }
+    }
+}
